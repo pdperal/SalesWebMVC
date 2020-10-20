f44aa294c8a2e625424b50c06b1d3456fd8e94f2
Pedro Peral <pep.90@hotmail.com>
2020-10-19 22:08:50 -0300

    Adiciona formatação nas propriedades da entidade Seller

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMVC.Models
@@ -9,7 +10,12 @@ namespace SalesWebMVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="0:dd/mm/yy")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }        
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
