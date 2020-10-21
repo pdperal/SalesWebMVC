f591d8fd391f8f58fdf5d3d4090fc55eeabc5a58
Pedro Peral <pep.90@hotmail.com>
2020-10-20 23:09:59 -0300

    Implementa filtro agrupado por departamento

--- SalesWebMVC/Services/SalesRecordService.cs
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -27,13 +27,34 @@ namespace SalesWebMVC.Services
             }
             if (maxDate.HasValue)
             {
-                result = result.Where(x => x.Date <= minDate.Value);
+                result = result.Where(x => x.Date <= maxDate.Value);
             }
 
             return await result
                 .Include(x => x.Seller)
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
                 .ToListAsync();
         }
     }
