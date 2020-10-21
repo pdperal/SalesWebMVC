16f0f674c9472d4a4d525b5bde6168646e4b3f79
Pedro Peral <pep.90@hotmail.com>
2020-10-20 22:38:05 -0300

    Implementa filtro de data para a busca de vendas.

---
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SalesRecordService _recordService;
+
+        public SalesRecordsController(SalesRecordService recordService)
+        {
+            _recordService = recordService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _recordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
